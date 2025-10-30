3cb37ce0c3ea9edb7463650a7c86b6d6d3b8b1f8
H.S <henrik1708@gmail.com>
2025-10-30 12:31:18 +0100

    Flyttet web innholdet fra tidligere til den nye mappen for komponenten i app-modulen for bedre struktur.

---
+++ develop/app/src/main/java/org/develop/web/Main.java
@@ -0,0 +1,48 @@
+package org.develop.web;
+
+import org.develop.domain.DatabaseSetup;
+import org.develop.web.service.Routes;
+import org.develop.service.UserService;
+import org.develop.Database.SQLDatabaseConnection;
+import org.develop.Database.DatabaseUserRepository;
+
+import io.javalin.Javalin;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Initialiser Database 
+        DatabaseSetup dbSetup = new DatabaseSetup(
+            "se25_G16",
+            "itstud.hiof.no",
+            "3306",
+            "gruppe16",
+            "Summer35"
+        );
+
+        System.out.println("Konfigurerer database...");
+        System.out.println(dbSetup.showDatabaseInfo());
+        
+        // Koble til Database (Krever skolenettverk / VPN)
+        SQLDatabaseConnection dbConnection = new SQLDatabaseConnection(
+            dbSetup.getDbUrl(),
+            dbSetup.getDbUsername(),
+            dbSetup.getDbPassword()
+        );
+        
+        // Initialiser UserService med repository 
+        DatabaseUserRepository userRepository = new DatabaseUserRepository(dbConnection);
+        UserService.initialize(userRepository);
+        
+        // Starter Javalin Web Server 
+        System.out.println("Starter web server på port 7000...");
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/static"); 
+        }).start(7000);
+
+        // Konfigurer alle routes 
+        Routes.configureRoutes(app);
+        
+        System.out.println("Web server kjører på http://localhost:7000");
+    }
+}
---
+++ develop/app/src/main/java/org/develop/web/service/WeatherController.java
@@ -0,0 +1,14 @@
+package org.develop.web.service;
+
+public class WeatherController {
+
+    Boolean isWeatherGood = false;
+
+    public String checkWeather() {
+        if(isWeatherGood) {
+            return "Været er bra for reise!";
+        } else {
+            return "Været er dårlig, vær forsiktig!";
+        }
+    }
+}
